9a605a5f8c9c61a767672714f39ac9bb306797b9
Raevinj1 <raevinjhnsn504@gmail.com>
2022-04-05 12:28:16 -0500

    Create new user working and connected to profile

--- app/src/main/java/com/example/scholarlysavings/UserInfo.java
+++ app/src/main/java/com/example/scholarlysavings/UserInfo.java
@@ -5,7 +5,7 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
-@ParseClassName("Information")
+@ParseClassName("User")
 public class UserInfo extends ParseObject{
     public static final String KEY_NAME = "Name";
     public static final String KEY_PHONE = "Phone";
